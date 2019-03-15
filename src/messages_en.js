04f9e82b5d94faf052beb01db0ea00072b386838
Annemhv <anne@aksios.fi>
2019-03-15 14:48:41 +0200

    Commit 3 15.03.2019

--- src/messages_en.js
+++ src/messages_en.js
@@ -19,7 +19,41 @@ const mes_en = () => {
     "id": "UNLOCK",
     "id": "areyounot",
     "reset.pws": "RESET MY PASSWORD",
-    "resetyour.pws": "RESET YOUR PASSWORD"
+    "resetyour.pws": "RESET YOUR PASSWORD",
+    "thank.you": "Hey! Thank you for checking out our app.",
+    "ready.yet": "It’s not quite ready yet, but we are working hard and it will be ready in approximately:",
+    "subscrib_to_email": "If you would like to be notified when the app is ready, you can subscribe to our e-mail list.",
+    "subscribe": "SUBSCRIBE",
+    "error": "Sorry but we could not find the page you are looking for",
+    "search": "search",
+    "gotodashboard": "Go back to dashboard",
+    "broketheinternet": "Well, you broke the internet!",
+	"justkidding": "Just kidding, looks like we have an internal issue, please try again in couple minutes",
+    "reportproblem": "Report this problem",
+    "heretohelp": "We're here to help",
+    "feqquestions": "Frequently asked questions",
+    "expandmore": "expand_more",
+    "helpoutline": "help_outline",
+
+    "invoice": "INVOICE",
+    "invoicedate": "INVOICE DATE",
+    "duedate": "DUE DATE",
+    "service": "SERVICE",
+    "unit": "UNIT",
+    "unitprice": "UNIT PRICE",
+    "quantity": "QUANTITY",
+    "total": "TOTAL",
+    "subtotal": "SUBTOTAL",
+    "tax": "TAX",
+    "discount": "DISCOUNT",
+    "subtotal": "SUBTOTAL",
+    "paybusiness": "Please pay within 15 days. Thank you for your business.",
+    "french": "In condimentum malesuada efficitur. Mauris volutpat placerat auctor. Ut ac congue dolor. Quisque scelerisque lacus sed feugiat fermentum. Cras aliquet facilisis pellentesque. Nunc hendrerit quam at leo commodo, a suscipit tellus dapibus. Etiam at felis volutpat est mollis lacinia. Mauris placerat sem sit amet velit mollis, in porttitor ex finibus. Proin eu nibh id libero tincidunt lacinia et eget eros.",
+    "phone": "Phone: ",
+    "email": "Email: ",
+    "web": "Web: ",
+    "client": "CLIENT"
+
 }
 );
     };
